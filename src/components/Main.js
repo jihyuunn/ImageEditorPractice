0275f062613d84ad0b63c0c22534a3633cd53100
jihyuunn <791004_@likelion.org>
2020-03-27 10:08:25 +0900

    ❗️react-image-crop 추가

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,13 @@
 import React, {useState, useRef, useEffect} from 'react'
 import styled from 'styled-components'
+import ReactCrop from 'react-image-crop'
+import 'react-image-crop/dist/ReactCrop.css'
+
+const MainCanvas = styled.div`
+    .hidden {
+        display: none;
+    }
+`
 
 const Main = () => {
     const storage = window.firebase.storage();
@@ -10,33 +18,34 @@ const Main = () => {
     const canvas = useRef()
     const image = useRef()
     let ctx
+
     useEffect(() => {
         console.log(canvas.current)
         ctx = canvas.current.getContext("2d")
         
     }, [canvas])
+
     const [file, setFile] = useState('')
-    function fileHandler(e) {
+    async function fileHandler(e) {
         setImageAsFile(e.target.files[0])
         const reader = new FileReader();
         reader.onloadend = function(event) {
-            console.log(event.target.result)
+            setCropInfo({...cropInfo, src: reader.result})
             setFile(reader.result)
-            ctx.drawImage(file, 0, 0)
-            ctx.front = "40px Courier"
         }
-        reader.readAsDataURL(e.target.files[0])
-        setFile(reader.result)
+        await reader.readAsDataURL(e.target.files[0])
+        // setFile(reader.result)
         setLoad(true)
-        // image.current.onload = () => {
-        //     ctx.drawImage(file, 0, 0)
-        //     ctx.front = "40px Courier"
-        //     ctx.fillText(text, 210, 75)
-        // }
+        await canvasHandler()
     }
-    useEffect(() => {
-        console.log(file)
-    }, [load])
+
+    function canvasHandler() {
+        image.current.onload = () => {
+            ctx.drawImage(image.current, 0, 0)
+            ctx.front = "40px Courier"
+            // ctx.fillText(text, 210, 75)
+        }
+    }
 
     const firebaseHandler = e => {
         e.preventDefault();
@@ -57,8 +66,73 @@ const Main = () => {
            })
         })
     }
+
+    const [cropInfo, setCropInfo] = useState({
+        scr: null,
+        crop: {
+            unit: '%',
+            width: 50,
+            height: 100,
+            // aspect: 16/9
+        },
+        croppedImgUrl: null
+    })
+    const onImageLoaded = image => {
+        this.imageRef = image
+    }
+    
+    const onCropChange = (crop) => {
+        setCropInfo({...cropInfo, crop });
+    }
+    
+    const onCropComplete = crop => {
+        if (image && crop.width && crop.height) {
+            const croppedImageUrl = getCroppedImg(image.current, crop)
+            setCropInfo({...cropInfo, croppedImgUrl: croppedImageUrl })
+        }
+    }
+
+    const getCroppedImg = (image, crop) => {
+        const scaleX = image.naturalWidth / image.width;
+        const scaleY = image.naturalHeight / image.height;
+        canvas.width = crop.width;
+        canvas.height = crop.height;
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(
+            image,
+            crop.x * scaleX,
+            crop.y * scaleY,
+            crop.width * scaleX,
+            crop.height * scaleY,
+            0,
+            0,
+            crop.width,
+            crop.height
+         )
+    
+        const reader = new FileReader()
+        canvas.toBlob(blob => {
+            reader.readAsDataURL(blob)
+            reader.onloadend = () => {
+                this.dataURLtoFile(reader.result, 'cropped.jpg')
+            }
+        })
+    }
+    const dataURLtoFile = (dataurl, filename) => {
+        let arr = dataurl.split(','),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), 
+        n = bstr.length, 
+        u8arr = new Uint8Array(n);
+            
+        while(n--){
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        let croppedImage = new File([u8arr], filename, {type:mime});
+        setCropInfo({...cropInfo, croppedImage: croppedImage }) 
+    }
     return (
-        <div>
+        <MainCanvas>
             <form onSubmit={firebaseHandler}>
                 <label>
                     <input type='file' onChange={fileHandler}/>
@@ -67,9 +141,16 @@ const Main = () => {
             </form>
             <div>
                 <canvas ref={canvas} width={600} height={400} />
-                {load ? <img ref={image} src={file} alt="uploadedImg"/>:null}
+                {load ? <img ref={image} src={file} alt="uploadedImg" className="hidden"/>:null}
+                {cropInfo.src && (
+                <ReactCrop
+                  src={cropInfo.src}
+                  crop={cropInfo.crop}
+                  onChange={onCropChange}
+                 /> 
+                )}
             </div>
-        </div>
+        </MainCanvas>
     )
 }
 
