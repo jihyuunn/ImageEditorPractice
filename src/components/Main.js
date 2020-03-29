79e11badc1b5d4a780a0aa5cffe9b7f89b19a303
jihyuunn <791004_@likelion.org>
2020-03-29 22:51:26 +0900

    ❓starting cropper

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,8 +2,92 @@ import React, {useState, useRef, useEffect} from 'react'
 import styled from 'styled-components'
 
 const MainCanvas = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
     .hidden {
         display: none;
+    }
+    .resize-container {
+    position: relative;
+    display: inline-block;
+    cursor: move;
+    margin: 0 auto;
+    }
+
+    .resize-container img {
+        display: block
+    }
+
+    .resize-container:hover img,
+    .resize-container:active img {
+        outline: 2px dashed rgba(222,60,80,.9);
+    }
+    .resize-handle-ne,
+    .resize-handle-ne,
+    .resize-handle-se,
+    .resize-handle-nw,
+    .resize-handle-sw {
+        position: absolute;
+        display: block;
+        width: 10px;
+        height: 10px;
+        background: rgba(222,60,80,.9);
+        z-index: 999;
+    }
+
+    .canvas_container {
+        position: relative;
+    }
+    .canvas {
+        width: 100%;
+        height: 100%;
+    }
+    .cropper {
+        width: 1000px;
+        height: 700px;
+        position: absolute;
+        top: 0;
+        left: 0;
+        background-color: rgba(0,0,0,.7);
+    }
+    .crop_square {
+        position: absolute;
+        cursor: move;
+        left: 101px;
+        top: 121px;
+        width: 811px;
+        height: 406px;
+        background-color: gold;
+    }
+    .crop_square_margin {
+        width: 10px;
+        height: 10px;
+        background-color: white;
+        border-radius: 2px;
+        border: 1px solid #333;
+        opacity: 0.8;
+        position: absolute;
+    }
+    .ne {
+        top: -5px;
+        right: -5px;
+        cursor: ne-resize;
+    }
+    .se {
+        bottom: -5px;
+        right: -5px;
+        cursor: se-resize;
+    }
+    .sw {
+        bottom: -5px;
+        left: -5px;
+        cursor: sw-resize;
+    }
+    .nw {
+        top: -5px;
+        left: -5px;
+        cursor: nw-resize;
     }
 `
 
@@ -91,18 +175,29 @@ const Main = () => {
         })
     }
 
-            
+
     return (
         <MainCanvas>
+            <div className="canvas_container">
+                <canvas ref={canvas} width={600} height={400} className="hidden" />
+                <div className="canvas">
+                    <div className="cropper">
+                        <div className="crop_square">
+                            <div className="crop_square_margin ne"></div>
+                            <div className="crop_square_margin se"></div>
+                            <div className="crop_square_margin sw"></div>
+                            <div className="crop_square_margin nw"></div>
+                        </div>
+                    </div>
+                    {load ? <img ref={image} src={file} alt="uploadedImg" />:null}  
+                </div>
+            </div>
             <form onSubmit={firebaseHandler}>
                 <label>
                     <input type='file' onChange={fileHandler}/>
                 </label>
                 <input type='submit' value='submit' />
             </form>
-            <div>
-                <canvas ref={canvas} width={600} height={400} className="hidden" />
-            </div>
         </MainCanvas>
     )
 }
