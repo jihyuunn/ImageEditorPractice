e00cb91c20abffbc1bab4c788b294669e77e3987
jihyuunn <791004_@likelion.org>
2020-03-29 22:15:34 +0900

    ❗️remove react-image-crop

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,5 @@
 import React, {useState, useRef, useEffect} from 'react'
 import styled from 'styled-components'
-import ReactCrop from 'react-image-crop'
-import 'react-image-crop/dist/ReactCrop.css'
 
 const MainCanvas = styled.div`
     .hidden {
@@ -30,7 +28,6 @@ const Main = () => {
         setImageAsFile(e.target.files[0])
         const reader = new FileReader();
         reader.onloadend = function(event) {
-            setCropInfo({...cropInfo, src: reader.result})
             setFile(reader.result)
         }
         await reader.readAsDataURL(e.target.files[0])
@@ -67,31 +64,6 @@ const Main = () => {
         })
     }
 
-    const [cropInfo, setCropInfo] = useState({
-        scr: null,
-        crop: {
-            unit: '%',
-            width: 50,
-            height: 100,
-            // aspect: 16/9
-        },
-        croppedImgUrl: null
-    })
-    const onImageLoaded = image => {
-        this.imageRef = image
-    }
-    
-    const onCropChange = (crop) => {
-        setCropInfo({...cropInfo, crop });
-    }
-    
-    const onCropComplete = crop => {
-        if (image && crop.width && crop.height) {
-            const croppedImageUrl = getCroppedImg(image.current, crop)
-            setCropInfo({...cropInfo, croppedImgUrl: croppedImageUrl })
-        }
-    }
-
     const getCroppedImg = (image, crop) => {
         const scaleX = image.naturalWidth / image.width;
         const scaleY = image.naturalHeight / image.height;
@@ -118,19 +90,8 @@ const Main = () => {
             }
         })
     }
-    const dataURLtoFile = (dataurl, filename) => {
-        let arr = dataurl.split(','),
-        mime = arr[0].match(/:(.*?);/)[1],
-        bstr = atob(arr[1]), 
-        n = bstr.length, 
-        u8arr = new Uint8Array(n);
+
             
-        while(n--){
-            u8arr[n] = bstr.charCodeAt(n);
-        }
-        let croppedImage = new File([u8arr], filename, {type:mime});
-        setCropInfo({...cropInfo, croppedImage: croppedImage }) 
-    }
     return (
         <MainCanvas>
             <form onSubmit={firebaseHandler}>
@@ -140,15 +101,7 @@ const Main = () => {
                 <input type='submit' value='submit' />
             </form>
             <div>
-                <canvas ref={canvas} width={600} height={400} />
-                {load ? <img ref={image} src={file} alt="uploadedImg" className="hidden"/>:null}
-                {cropInfo.src && (
-                <ReactCrop
-                  src={cropInfo.src}
-                  crop={cropInfo.crop}
-                  onChange={onCropChange}
-                 /> 
-                )}
+                <canvas ref={canvas} width={600} height={400} className="hidden" />
             </div>
         </MainCanvas>
     )
