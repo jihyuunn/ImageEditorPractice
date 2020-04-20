5ede8849cbdeb1b776c76dec0ac5a514c5cc1290
jihyuunn <791004_@likelion.org>
2020-04-20 16:55:44 +0900

    ❗️사이드 바에 컴포넌트 넣기

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
+import SideBarBox from './SideBarBox'
 
-const SideBar = () => {
+const SideBarContainer = styled.div`
+    width: 25%;
+    display: flex;
+    flex-direction: column;
+`
+
+const SideBar = ({ size, sizeHandler }) => {
     return (
-        <div>
-            
-        </div>
+        <SideBarContainer>
+            <SideBarBox title='SIZES'>
+                <lable>
+                    <input type='radio' value='tall' checked={size.tall} onChange={e => sizeHandler(e)} />
+                    Tall
+                </lable>
+                <lable>
+                    <input type='radio' value='wide' checked={size.wide} onChange={e => sizeHandler(e)} />
+                    Wide
+                </lable>
+            </SideBarBox>
+            <SideBarBox title='TEXT'>
+                <li>Font</li>
+                <li>Font size</li>
+            </SideBarBox>
+        </SideBarContainer>
     )
 }
 
